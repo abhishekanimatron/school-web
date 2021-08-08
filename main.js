59b8a23d90e41a39acd97faea755e46575648c3d
abhishekanimatron <abhishekverma297ab@gmail.com>
2021-08-08 15:55:56 +0530

    done

---
+++ main.js
@@ -0,0 +1,27 @@
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
+//for achievements
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+  counter("count1", 100, 289, 2000);
+  counter("count2", 0, 90, 1000);
+  counter("count3", 0, 177, 1500);
+});
